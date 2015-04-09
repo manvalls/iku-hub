c7a48c2516261143d971898ba3d879883ef99169
Manuel Valls Fernández <manolo@vlrz.es>
2015-04-09 23:32:08 +0200

    Fixes #2

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var Su = require('u-su'),
     wrap = require('y-walk').wrap,
     
     peers = Su(),
+    remotePeers = Su(),
+    roomPeers = Su(),
     emitter = Su(),
     rid = Su(),
     pids = Su(),
@@ -18,18 +20,27 @@ var Su = require('u-su'),
 
 // Room object
 
-function Room(){
+function Room(pm){
   Emitter.Target.call(this,emitter);
   
   this[rid] = unique();
-  this[peers] = {};
   this[total] = 0;
   
+  this[peers] = {};
+  this[remotePeers] = {};
+  this[roomPeers] = {};
+  
+  if(pm) pm.on('peer',roomOnPeer,this);
   plugins.give('room',this);
 }
 
+// Room utils
+
 function broadcast(room,msg,pid){
-  var keys,i,j;
+  var keys,i,j,peer;
+  
+  if(msg[room[rid]]) return;
+  msg[room[rid]] = true;
   
   if(typeof pid == 'string'){
     
@@ -39,9 +50,11 @@ function broadcast(room,msg,pid){
       if(i !== pid) room[peers][i].give('msg',msg);
     }
     
+    pid = null;
+    
   }else{
     
-    keys = pid || Object.keys(room[peers]);
+    keys = Object.keys(room[peers]);
     for(j = 0;j < keys.length;j++){
       i = keys[j];
       room[peers][i].give('msg',msg);
@@ -49,6 +62,150 @@ function broadcast(room,msg,pid){
     
   }
   
+  peer = pid;
+  
+  if(peer){
+    
+    keys = Object.keys(room[roomPeers]);
+    for(j = 0;j < keys.length;j++){
+      i = keys[j];
+      if(room[roomPeers][i] != peer) room[roomPeers][i].give('msg',msg);
+    }
+    
+  }else{
+    
+    keys = Object.keys(room[roomPeers]);
+    for(j = 0;j < keys.length;j++){
+      i = keys[j];
+      room[roomPeers][i].give('msg',msg);
+    }
+    
+  }
+  
+}
+
+function getPeerList(room){
+  var ret;
+  
+  ret = Object.keys(room[peers]);
+  ret = ret.concat(Object.keys(room[remotePeers]));
+  
+  return ret;
+}
+
+function send(msg,room){
+  if(room[peers][msg.to]) room[peers][msg.to].give('msg',msg);
+  else if(room[remotePeers][msg.to]) room[remotePeers][msg.to].give('msg',msg);
+}
+
+function findOrDelete(pid,room,peer){
+  var keys,i,j,rp;
+  
+  // TODO: multiple pids support
+  
+  if(room[remotePeers][pid]) return;
+  
+  keys = Object.keys(room[roomPeers]);
+  for(j = 0;j < keys.length;j++){
+    i = keys[j];
+    rp = room[roomPeers][i];
+    
+    if(rp[peers][pid]){
+      room[remotePeers][pid] = rp;
+      return;
+    }
+  }
+  
+  broadcast(room,{
+    type: 'bye',
+    rid: room[rid],
+    pids: [pid]
+  },peer);
+  
+  if(!--room[total]) room[emitter].set('empty');
+}
+
+// Room listeners
+
+function roomOnPeer(peer,c,room){
+  var id = Su(),
+      pids;
+  
+  room[roomPeers][id] = peer;
+  peer[peers] = {};
+  
+  peer.on('msg',roomOnPeerMsg,room,id);
+  peer.once('closed',roomOncePeerClosed,room,id);
+  
+  pids = getPeerList(room);
+  if(pids.length) peer.give('msg',{
+    type: 'hi',
+    pids: pids
+  });
+}
+
+function roomOnPeerMsg(msg,c,room,id){
+  var i,pid;
+  
+  if(typeof msg != 'object') return;
+  
+  if(msg.from){
+    if(msg.to == 'all') broadcast(room,msg,this);
+    else send(msg,room);
+  }else if(msg.rid) switch(msg.type){
+    
+    case 'hi':
+      if(!(msg.pids instanceof Array)) return;
+      
+      for(i = 0;i < msg.pids.length;i++){
+        pid = msg.pids[i];
+        this[peers][pid] = true;
+        
+        if(!(room[remotePeers][pid] || room[peers][pid])){
+          room[remotePeers][pid] = this;
+          
+          if(!room[total]) room[emitter].unset('empty');
+          room[total]++;
+        }
+      }
+      
+      msg.rid = room[rid];
+      broadcast(room,msg,this);
+      
+      break;
+    
+    case 'bye':
+      if(!(msg.pids instanceof Array)) return;
+      
+      for(i = 0;i < msg.pids.length;i++){
+        pid = msg.pids[i];
+        delete this[peers][pid];
+        
+        if(room[remotePeers][pid] == this){
+          delete room[remotePeers][pid];
+          findOrDelete(pid,room,this);
+        }
+      }
+      
+      break;
+    
+  }else broadcast(room,msg,this);
+  
+}
+
+function roomOncePeerClosed(e,c,room,id){
+  var keys,j,i;
+  
+  delete room[roomPeers][id];
+  
+  keys = Object.keys(this[peers]);
+  for(j = 0;j < keys.length;j++){
+    i = keys[j];
+    if(room[remotePeers][i] == this){
+      delete room[remotePeers][i];
+      findOrDelete(i,room);
+    }
+  }
 }
 
 Room.prototype = new Emitter.Target();
@@ -57,7 +214,7 @@ Room.prototype.constructor = Room;
 Object.defineProperties(Room.prototype,{
   
   add: {value: function(p){
-    var pid,peer,msg,i,j,keys;
+    var pid,peer,msg,i,j;
     
     if(p[pids][this[rid]]) return;
     
@@ -73,23 +230,24 @@ Object.defineProperties(Room.prototype,{
     peer.give('msg',{
       type: 'hi',
       rid: this[rid],
-      pids: keys = Object.keys(this[peers])
+      pids: getPeerList(this)
     });
-    
-    this[peers][pid] = peer;
-    peer.once('closed',room_onceClosed,this,p);
     
     broadcast(this,{
       type: 'hi',
       rid: this[rid],
       pids: [pid]
-    },keys);
+    });
     
+    this[peers][pid] = peer;
+    peer.once('closed',room_onceClosed,this,p);
   }},
   
   remove: {value: function(p){
     var pid = p[pids][this[rid]],
         keys,i,j,msg;
+    
+    if(!pid) return;
     
     if(!this[peers][pid].is('closed')) this[peers][pid].give('msg',{
       type: 'bye',
@@ -101,13 +259,7 @@ Object.defineProperties(Room.prototype,{
     delete p[rooms][this[rid]];
     delete this[peers][pid];
     
-    broadcast(this,{
-      type: 'bye',
-      rid: this[rid],
-      pids: [pid]
-    });
-    
-    if(!--this[total]) this[emitter].set('empty');
+    findOrDelete(pid,this);
   }},
   
   give: {value: function(type,data){
@@ -195,7 +347,7 @@ function onPeer(peer,cbc,server){
 function onMsg(msg,cbc,ep){
   var room;
   
-  if(!msg) return;
+  if(typeof msg != 'object') return;
   
   if(msg.rid){
     room = ep[rooms][msg.rid];
@@ -204,10 +356,7 @@ function onMsg(msg,cbc,ep){
     msg.from = ep[pids][msg.rid];
     
     if(msg.to == 'all') return broadcast(room,msg,msg.from);
-    
-    if(!room[peers][msg.to]) return;
-    room[peers][msg.to].give('msg',msg);
-    
+    send(msg,room);
   }else if(msg.type == 'msg') ep[emitter].give('msg',msg.data);
   else serverPlugins.give(msg.type,[msg.data,ep[emitter]]);
   
