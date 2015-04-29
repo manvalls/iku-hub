e4609886015159a077ce78c02c4f3655757dc377
Manuel Valls Fernández <manolo@vlrz.es>
2015-04-29 10:53:19 +0200

    rtc plugin rewrite

--- plugins/rtc.js
+++ plugins/rtc.js
@@ -2,22 +2,21 @@ var Su = require('u-su'),
     unique = require('u-rand').unique,
     wrap = require('y-walk').wrap,
     
-    walk = require('u-proto/walk'),
-    until = require('u-proto/until'),
+    on = require('u-proto/on'),
     
     Client = require('../client.js'),
     rtc = require('./rtc/poly.js'),
     
-    event = '3M9E3T-F9qsz',
+    pref = '3M9E3T-F9qsz-',
     
-    streams = Su();
+    pcs = Su();
 
-function* handleIce(peer,sid){
-  var e = yield this[until]('icecandidate'),
-      ice;
+// Callbacks
+
+function onIce(e,c,peer,cid){
+  var ice;
   
   if(!(e && e.candidate)) return;
-  this[walk](handleIce,arguments);
   
   ice = {
     candidate: e.candidate.candidate,
@@ -25,35 +24,107 @@ function* handleIce(peer,sid){
     sdpMLineIndex: e.candidate.sdpMLineIndex
   };
   
-  peer.give(event,{
-    type: 'ice',
-    sid: sid,
+  peer.give(pref + 'ice',{
+    cid: cid,
     ice: ice
   });
   
 }
 
-function* closePc(pc){
-  yield this.until('closed');
+function onceClosed(e,c,pc){
   pc.close();
 }
 
-Object.defineProperty(Client.Peer.prototype,'sendStream',{value: wrap(function*(stream,opts){
-  var pc = new rtc.Pc(opts || rtc.PcOpts),
-      sid = unique(),
-      offer,msg;
+function onStream(e,c,emitter){
+  emitter.give('stream',e.stream);
+}
+
+Client.peerPlugins.on(pref + 'new-pc',function(e){
+  var data = e[0],
+      emitter = e[1],
+      peer = emitter.target,
+      pc;
   
-  this.give(event,{
-    type: 'new',
-    opt: opts,
-    sid: sid
+  peer[pcs] = peer[pcs] || {};
+  if(peer[pcs][data.cid]) return;
+  
+  pc = peer[pcs][data.cid] = new rtc.Pc(data.opt || rtc.PcOpts);
+  pc[on]('icecandidate',onIce,peer,data.cid);
+  pc[on]('addstream',onStream,emitter);
+  peer.once('closed',onceClosed,pc);
+  
+});
+
+Client.peerPlugins.on(pref + 'ice',function(e){
+  var data = e[0],
+      emitter = e[1],
+      peer = emitter.target,
+      pc;
+  
+  peer[pcs] = peer[pcs] || {};
+  pc = peer[pcs][data.cid];
+  if(!pc) return;
+  
+  rtc.ice(pc,new rtc.Ice(data.ice));
+  
+});
+
+Client.peerPlugins.on(pref + 'offer',function*(e){
+  var data = e[0],
+      emitter = e[1],
+      peer = emitter.target,
+      pc;
+  
+  peer[pcs] = peer[pcs] || {};
+  pc = peer[pcs][data.cid];
+  if(!pc) return;
+  
+  yield rtc.remote(pc,new rtc.Sd(data.offer));
+  answer = yield rtc.answer(pc);
+  yield rtc.local(pc,answer);
+  
+  peer.give(pref + 'answer',{
+    cid: data.cid,
+    answer: {
+      type: answer.type,
+      sdp: answer.sdp
+    }
   });
   
-  this[streams] = this[streams] || {};
-  this[streams][sid] = pc;
+});
+
+Client.peerPlugins.on(pref + 'answer',function(e){
+  var data = e[0],
+      emitter = e[1],
+      peer = emitter.target,
+      pc;
   
-  pc[walk](handleIce,[this,sid]);
+  peer[pcs] = peer[pcs] || {};
+  pc = peer[pcs][data.cid];
+  if(!pc) return;
+  
+  rtc.remote(pc,new rtc.Sd(data.answer));
+  
+});
+
+// Streams
+
+Object.defineProperty(Client.Peer.prototype,'sendStream',{value: wrap(function*(stream,opts){
+  var pc = new rtc.Pc(opts || rtc.PcOpts),
+      cid = unique(),
+      offer,msg;
+  
+  this.give(pref + 'new-pc',{
+    opt: opts,
+    cid: cid
+  });
+  
+  this[pcs] = this[pcs] || {};
+  this[pcs][cid] = pc;
+  
+  pc[on]('icecandidate',onIce,this,cid);
   pc.addStream(stream);
+  this.once('closed',onceClosed,pc);
   
   offer = yield rtc.offer(pc);
   yield rtc.local(pc,offer);
@@ -63,73 +134,10 @@ Object.defineProperty(Client.Peer.prototype,'sendStream',{value: wrap(function*(
     sdp: offer.sdp
   };
   
-  this.give(event,{
-    type: 'offer',
-    sid: sid,
+  this.give(pref + 'offer',{
+    cid: cid,
     offer: msg
   });
   
 })});
-
-Client.peerPlugins.walk(function* cb(){
-  var args,msg,peer,emitter,pc,e,answer;
-  
-  args = yield this.until(event);
-  this.walk(cb);
-  
-  msg = args[0];
-  emitter = args[1];
-  peer = emitter.target;
-  
-  peer[streams] = peer[streams] || {};
-  
-  switch(msg.type){
-    
-    case 'new':
-      if(peer[streams][msg.sid]) return;
-      
-      pc = peer[streams][msg.sid] = new rtc.Pc(msg.opt || rtc.PcOpts);
-      pc[walk](handleIce,[peer,msg.sid]);
-      emitter.target.walk(closePc,[pc]);
-      
-      e = yield pc[until]('addstream');
-      emitter.give('stream',e.stream);
-      
-      break;
-    
-    case 'ice':
-      if(!(pc = peer[streams][msg.sid])) return;
-      
-      rtc.ice(pc,new rtc.Ice(msg.ice));
-      
-      break;
-    
-    case 'offer':
-      if(!(pc = peer[streams][msg.sid])) return;
-      
-      yield rtc.remote(pc,new rtc.Sd(msg.offer));
-      answer = yield rtc.answer(pc);
-      yield rtc.local(pc,answer);
-      
-      peer.give(event,{
-        type: 'answer',
-        sid: msg.sid,
-        answer: {
-          type: answer.type,
-          sdp: answer.sdp
-        }
-      });
-      
-      break;
-    
-    case 'answer':
-      if(!(pc = peer[streams][msg.sid])) return;
-      
-      yield rtc.remote(pc,new rtc.Sd(msg.answer));
-      
-      break;
-    
-  }
-  
-});
 
