1ba39f047b37d2d566684712a8e91f0982b7ebf7
Manuel Valls Fernández <manolo@vlrz.es>
2015-05-01 23:08:35 +0200

    Added RTCDC support

--- plugins/rtc.js
+++ plugins/rtc.js
@@ -3,13 +3,17 @@ var Su = require('u-su'),
     wrap = require('y-walk').wrap,
     
     on = require('u-proto/on'),
+    apply = require('u-proto/apply'),
     
     Client = require('../client.js'),
     rtc = require('./rtc/poly.js'),
     
     pref = '3M9E3T-F9qsz-',
     
-    pcs = Su();
+    labels = Su(),
+    pcs = Su(),
+    
+    offer;
 
 // Callbacks
 
@@ -36,7 +40,19 @@ function onceClosed(e,c,pc){
 }
 
 function onStream(e,c,emitter){
-  emitter.give('stream',e.stream);
+  var label;
+  
+  if(this[labels] && (label = this[labels][e.stream.id])) emitter.give('ST ' + label,e.stream);
+  else emitter.give('stream',e.stream);
+  
+}
+
+function onDC(e,c,emitter){
+  var label;
+  
+  if(e.channel.label) emitter.give('DC ' + e.channel.label,e.channel);
+  else emitter.give('datachannel',e.stream);
+  
 }
 
 Client.peerPlugins.on(pref + 'new-pc',function(e){
@@ -51,6 +67,7 @@ Client.peerPlugins.on(pref + 'new-pc',function(e){
   pc = peer[pcs][data.cid] = new rtc.Pc(data.opt || rtc.PcOpts);
   pc[on]('icecandidate',onIce,peer,data.cid);
   pc[on]('addstream',onStream,emitter);
+  pc[on]('datachannel',onDC,emitter);
   peer.once('closed',onceClosed,pc);
   
 });
@@ -107,15 +124,77 @@ Client.peerPlugins.on(pref + 'answer',function(e){
   
 });
 
+Client.peerPlugins.on(pref + 'label',function(e){
+  var data = e[0],
+      emitter = e[1],
+      peer = emitter.target,
+      pc;
+  
+  peer[pcs] = peer[pcs] || {};
+  pc = peer[pcs][data.cid];
+  if(!pc) return;
+  
+  pc[labels] = pc[labels] || {};
+  pc[labels][data.id] = data.label;
+  
+});
+
+// Utils
+
+offer = wrap(function*(pc,that,cid){
+  var offer,msg;
+  
+  offer = yield rtc.offer(pc);
+  yield rtc.local(pc,offer);
+  
+  msg = {
+    type: offer.type,
+    sdp: offer.sdp
+  };
+  
+  that.give(pref + 'offer',{
+    cid: cid,
+    offer: msg
+  });
+  
+});
+
+function getOpts(opts){
+  var pcOpts;
+  
+  if(!opts) return rtc.PcOpts;
+  
+  delete opts.label;
+  delete opts.id;
+  delete opts.negotiated;
+  
+  pcOpts = {};
+  pcOpts[apply](rtc.PcOpts);
+  pcOpts[apply](opts);
+}
+
 // Streams
 
-Object.defineProperty(Client.Peer.prototype,'sendStream',{value: wrap(function*(stream,opts){
-  var pc = new rtc.Pc(opts || rtc.PcOpts),
-      cid = unique(),
-      offer,msg;
+Object.defineProperty(Client.Peer.prototype,'sendStream',{value: function(stream,label,opts){
+  var cid = unique(),
+      pc,label,pcOpts;
+  
+  if(typeof label != 'string'){
+    opts = label;
+    label = null;
+  }
+  
+  pcOpts = getOpts(opts);
+  pc = new rtc.Pc(pcOpts);
   
   this.give(pref + 'new-pc',{
-    opt: opts,
+    opt: pcOpts,
+    cid: cid
+  });
+  
+  if(label) this.give(pref + 'label',{
+    id: stream.id,
+    label: label,
     cid: cid
   });
   
@@ -126,18 +205,38 @@ Object.defineProperty(Client.Peer.prototype,'sendStream',{value: wrap(function*(
   pc.addStream(stream);
   this.once('closed',onceClosed,pc);
   
-  offer = yield rtc.offer(pc);
-  yield rtc.local(pc,offer);
+  offer(pc,this,cid);
   
-  msg = {
-    type: offer.type,
-    sdp: offer.sdp
-  };
+}});
+
+// DataChannels
+
+Object.defineProperty(Client.Peer.prototype,'createDC',{value: function(label,opts){
+  var cid = unique(),
+      pc,label,pcOpts,dc;
   
-  this.give(pref + 'offer',{
-    cid: cid,
-    offer: msg
+  if(typeof label != 'string'){
+    opts = label;
+    label = '';
+  }
+  
+  pcOpts = getOpts(opts);
+  pc = new rtc.Pc(pcOpts);
+  
+  this.give(pref + 'new-pc',{
+    opt: pcOpts,
+    cid: cid
   });
   
-})});
+  this[pcs] = this[pcs] || {};
+  this[pcs][cid] = pc;
+  
+  pc[on]('icecandidate',onIce,this,cid);
+  dc = pc.createDataChannel(label,pcOpts);
+  
+  this.once('closed',onceClosed,pc);
+  offer(pc,this,cid);
+  
+  return dc;
+}});
 
